2713f5a745fea6295b3b66f6b8e6a5af69578308
ZumbaIdol <david.a.foote@gmail.com>
2019-10-17 11:02:40 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/students', to: 'views#students', as: 'index'
+  get '/students', to: 'students#students', as: 'index'
 end
